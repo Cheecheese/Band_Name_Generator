d1e59873f78a7c9fe618c658c4d6b6ef441fd1e2
cheecheese <chickpeakraken@protonmail.com>
2022-12-13 12:16:50 -0800

    Refresh course for Python

---
+++ main.py
@@ -0,0 +1,15 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+print("Welcome to the band name generator.")
+
+city = input("Which city did you grow up in?\n ")
+
+pet = input("What is your first pet name?\n ")
+
+colour = input("What is your favourite colour?\n ")
+
+print("Your band name could be: " + city + " " + pet + " " + colour)
+
